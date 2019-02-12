dc2992e12a8d6aa86ae8e1be90578dc91ebd3040
Hayk <hsimonyan09@gmail.com>
2019-02-12 20:01:36 +0400

    challenges

---
+++ Challenges/average-pair.js
@@ -0,0 +1,46 @@
+/*** Multiple Pointers - averagePair ***/ 
+
+// Write a function called averagePair. Given a sorted array of integers and a target average, 
+// determine if there is a pair of values in the array where the average of the pair equals the 
+// target average. There may be more than one pair that matches the average
+
+// Bonus Contrains:
+// Time: O(N)
+// Space: O(1)
+// Sample Input:
+//      averagePair([1,2,3], 2.5); // true
+//      averagePair([1,3,3,5,6,7,10,12,19], 8); // true
+//      averagePair([-1,0,3,4,5,6], 4.1); // false
+//      averagePair([], 4); // false
+
+// my solution
+const averagePair = (arr, avg) => {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while(left < right) {
+        if((arr[left]+arr[right])/2 == avg) {
+            return console.log('true');
+        }
+        right--;
+        left++;
+    }
+    
+    return console.log('false');
+}
+
+averagePair([3,6,9,8,7], 5.69);
+
+
+// colts solution
+function averagePair(arr, num){
+    let start = 0;
+    let end = arr.length - 1;
+
+    while(start < end) {
+        if (arr[start] + arr[end] / 2 === num) return true;
+        start++;
+        end--;
+    }
+    return false;
+}
---
+++ Challenges/same-frequency.js
@@ -0,0 +1,79 @@
+/***    Frequency Counter    ***/ 
+
+// Write a function called sameFrequency
+// Given two positive integers, find out if the two
+// numbers have the same frequency of digits.
+
+// Your solution MUST have the following complexities:
+// Time: O(N)
+
+//  sameFrequency(182, 281) // true
+//  sameFrequency(34, 14) // false
+//  sameFrequency(3589578, 5879385) // true
+//  sameFrequency(22, 222) // false
+
+//my solution
+const  sameFrequency = (int1, int2) => {
+    let str1 = int1.toString();
+    let str2 = int2.toString();
+
+    if(str1.length !== str2.length) {
+        return console.log('false')
+    }
+
+    frequencyCounter1 = {};
+    frequencyCounter2 = {};
+
+    for(let value of str1) {
+        frequencyCounter1[value] = (frequencyCounter1[value] || 0) + 1;
+    }
+    
+    for(let value of str2) {
+        frequencyCounter2[value] = (frequencyCounter2[value] || 0) + 1;
+    }
+
+    for(let key in frequencyCounter1) {
+        if(!(key in frequencyCounter2)) {
+            return console.log('false');
+        }
+        if(frequencyCounter1[key] !== frequencyCounter2[key]) {
+            return console.log('false');
+        }
+        return console.log('ture');
+    }
+
+    
+    console.log(frequencyCounter1, frequencyCounter2);
+    console.log(str1, str2);
+}
+
+
+sameFrequency(345, 453)
+
+
+
+// colts solution
+function sameFrequency(num1, num2){
+    num1 = num1.toString();
+    num2 = num2.toString();
+    
+    if(num1.length !== num2.length) return false;
+
+    let freqCount1 = {},
+        freqCount2 = {};
+    
+    for(let val of num1) {
+        freqCount1[val] = (freqCount1[val] || 0) + 1;
+    }
+    for(let val of num2) {
+        freqCount2[val] = (freqCount2[val] || 0) + 1;
+    }
+    
+    for(let key in freqCount1) {
+        if(freqCount1[key] !== freqCount2[key]) {
+            return false;
+        }
+    }
+
+    return true;
+}
